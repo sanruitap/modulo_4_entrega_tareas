ecb9a1ccd3d68c01ebe584ffc044b398a9d4570b
sanruitap <sanruitap@hotmail.com>
2024-10-15 05:11:22 -0500

    Quiero subier el App.jsx  ,  el title.jsx, image.jsx y App.css

---
+++ vite_tarea_3/vite_tarea_3/src/Components/image/image.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//import Image from '../pics/diomedesDiaz'
+
+
+const Image = () => {
+    
+    
+  return (
+    <img class="card-image"
+    src= 'https://elpilon.com.co/wp-content/uploads/2023/01/Diomedes.webp' >
+
+    </img>
+  )
+}
+
+
+export default Image
